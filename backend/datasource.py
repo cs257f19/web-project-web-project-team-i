51644efd40d3b643fa7c39ae5068bc851ff6d3f8
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-01 03:38:35 -0500

    modified

--- backend/datasource.py
+++ backend/datasource.py
@@ -45,13 +45,13 @@ class DataSource:
         RETURN:
             a string value of the Best Picture winner for the specified year
         '''
-
+        print(connection)
         yearOfRelease = year - 1
 
         try:
             cursor = connection.cursor()
+            print(cursor)
             query = "SELECT	picture FROM movies WHERE yearOfRelease = "  + yearOfRelease
-            print(query)
             cursor.execute(query)
             result = cursor.fetchall()
             picture = result[0]
