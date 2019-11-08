735c72c9e0dcff9bb22f737a32b463da94e8cc1b
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-08 13:50:29 -0600

    flask

--- webapp.py
+++ webapp.py
@@ -10,56 +10,17 @@ import sys
 app = flask.Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('WebPages/index.html')
+def homepage():
+    return render_template('index.html')
 
-@app.route('/fancier/')
-def itDoesHTML():
-    htmlStr = '<html lang="en">' + \
-        '<head>' + \
-        '  <title>Cool page</title>' + \
-        '</head>' + \
-        '<body>' + \
-        '  <h1>Welcome to CS 257</h1>' + \
-        '  <p>Please refrain from feeding the squirrels. &lt;test this&gt; </p>' + \
-        '</body>' + \
-        '</html>'
-    return htmlStr
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
 
-@app.route('/boring/')
-def boring():
-    return render_template('boring.html')
+@app.route('/actor/<year>')
+def actor():
+    return render_template('pages/actor.html', year=year)
 
-@app.route('/greet/<person>/')
-def greet(person):
-    return render_template('greet.html',
-                           person=person)
-
-@app.route('/fruit')
-def fruit():
-    myFruit = [
-        {'name': 'apple', 'rating': 7},
-        {'name': 'banana', 'rating': 5},
-        {'name': 'pear', 'rating': 4}
-    ]
-
-    return render_template('fruit.html',
-                           fruits=myFruit)
-
-@app.route('/fruitImg/')
-def fruitImg():
-    return render_template('fruitImg.html')
-
-@app.route('/authors/<author>')
-def get_author(author):
-    ''' What a dopey function! But it illustrates a Flask route with a parameter. '''
-    if author == 'Twain':
-        author_dictionary = {'last_name':'Twain', 'first_name':'Mark'}
-    elif author == 'Shakespeare':
-        author_dictionary = {'last_name':'Shakespeare', 'first_name':'William'}
-    else:
-        author_dictionary = {'last_name':'McBozo', 'first_name':'Bozo'}
-    return json.dumps(author_dictionary)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
