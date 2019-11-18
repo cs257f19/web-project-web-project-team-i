10c86e0964fe2afe38b599d40ea020cb40b30f25
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-17 23:28:20 -0600

    search function

--- backend/datasource.py
+++ backend/datasource.py
@@ -274,9 +274,9 @@ def main():
     item = "genre"
 
     result_winner = ds.get_winner(connection, year, category)
-    results.append(["result_winner", result_winner])
-    # result_film = ds.get_by_year(connection, year, category)
-    # results.append(["result_film", result_film])
+    # results.append(["result_winner", result_winner])
+    result_film = ds.get_by_year(connection, year, category)
+    results.append(["result_film", result_film])
     # result_item = ds.get_by_picture(connection, item, film)
     # results.append(["result_item", result_item])
     # result_pictures = ds.get_pictures(connection, 1927, 2018)
