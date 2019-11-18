b1ace1b6381152bfee7750a3e03956d9bccfaf3e
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-18 02:34:37 -0600

    search function

--- backend/datasource.py
+++ backend/datasource.py
@@ -217,7 +217,7 @@ class DataSource:
                 for picture in pictureArray:
                     # print(picture)
                     if "'" in picture:
-                        genre = genre
+                        genres = genres
                         # continue
                     else:
                         query = "SELECT subgenre FROM films WHERE picture = '"  + picture + "'"                        
