8aaab8cf1fd23042beb76a6bcb95119a9b9a1ff5
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-17 23:30:26 -0600

    search function

--- backend/datasource.py
+++ backend/datasource.py
@@ -145,8 +145,9 @@ class DataSource:
         try:
             if item == "genre":
                 query = "SELECT subgenre FROM films WHERE picture = '"  + picture + "'"
+                print(self.execute_query(connection, query))
                 if self.execute_query(connection, query) != []:
-                    print(self.execute_query(connection, query))
+                    
                     subgenre = self.execute_query(connection, query)[0][0]
                     if subgenre == "Drama" or subgenre == "NA":
                         query = "SELECT genre FROM films WHERE picture = '"  + picture + "'"
