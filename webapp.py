a058eae52545c6cc6ec38f224616a2e062eb80a9
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-17 22:57:16 -0600

    search function

--- webapp.py
+++ webapp.py
@@ -109,7 +109,7 @@ def pictures_by_genre(genre):
     for picture in pictures:
         if type(picture[0]) == str:
             quered_genre = ds.get_by_picture(connection, 'genre', picture[0])
-
+            results.append("picture", picture[0])
             results.append(quered_genre)
             # if quered_genre == genre:
                 # results.append({"picture": picture[0], "year":picture[1]})
