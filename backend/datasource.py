0f92c38ccc7830eb3ba565ede07d28ec13821b5b
Rie Kurita <Rie@eduroam-178-251.dyn.carleton.edu>
2019-11-18 12:04:34 -0600

    search function

--- backend/datasource.py
+++ backend/datasource.py
@@ -144,18 +144,21 @@ class DataSource:
         '''
         try:
             if item == "genre":
+                print('quered')
                 query = "SELECT subgenre FROM films WHERE picture = '"  + picture + "'"
-                
                 if self.execute_query(connection, query) != []:
-                    
+                    print('not empty')
                     subgenre = self.execute_query(connection, query)[0][0]
                     if subgenre == "Drama" or subgenre == "NA":
+                        print('subgenre is drama')
                         query = "SELECT genre FROM films WHERE picture = '"  + picture + "'"
                         genre = self.execute_query(connection, query)[0][0]
                         result = genre
                     else:
+                        print('subgenre is NOT drama')
                         result = subgenre
                 else:
+                    print('EMPTY')
                     query = "SELECT genre FROM films WHERE picture = '"  + picture + "'"
                     genre = self.execute_query(connection, query)[0][0]
                     result = genre
